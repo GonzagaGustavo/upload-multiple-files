1d9764f0bd3fc840af8784e818cbf49ee5851c96
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2023-09-10 23:26:04 -0300

    showing percentage with  ffmpeg video processor

--- backend/src/uploadHandler.ts
+++ backend/src/uploadHandler.ts
@@ -1,9 +1,15 @@
 import BusBoy from "busboy";
-import { createWriteStream } from "node:fs";
-import { ServerResponse } from "node:http";
-import { join } from "node:path";
-import { pipeline } from "node:stream/promises";
+import { spawn } from "node:child_process";
+import { createWriteStream, unlinkSync } from "node:fs";
+import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
+import path, { join } from "node:path";
 import { Server } from "socket.io";
+import internal from "node:stream";
+import * as tmp from "tmp";
+import { pipeline } from "node:stream/promises";
+import progressStream from "ffmpeg-progress-stream";
+import ffprobe from "ffprobe";
+import ffprobeStatic from "ffprobe-static";
 
 export default class UploadHandler {
   private io: Server;
@@ -17,9 +23,9 @@ export default class UploadHandler {
   registerEvents(headers: any, onFinish: any) {
     const busboy = BusBoy({ headers });
 
-    busboy.on("file", this.onFile.bind(this));
+    busboy.on("file", this.onFile.bind(this, onFinish));
 
-    busboy.on("finish", onFinish);
+    // busboy.on("finish", onFinish);
 
     return busboy;
   }
@@ -41,21 +47,63 @@ export default class UploadHandler {
     return handleData.bind(this);
   }
 
-  private async onFile(fieldname: string, file: string, data: any) {
+  private async onFile(
+    onFinish: any,
+    name: string,
+    stream: internal.Readable,
+    info: BusBoy.FileInfo
+  ) {
     const saveFileTo = join(
       __dirname,
       "../",
       "download",
-      new Date().valueOf() + data.filename
-    );
-    console.log("Uploading to", saveFileTo);
-
-    await pipeline(
-      file,
-      this.handleFileBytes.apply(this, [data.filename]),
-      createWriteStream(saveFileTo)
+      `${new Date().valueOf()}_${name}`
     );
 
-    console.log("File " + data.filename + " finished!");
+    const tempFilePath = tmp.tmpNameSync({
+      postfix: path.extname(info.filename),
+    });
+
+    await pipeline(stream, createWriteStream(tempFilePath));
+
+    const frames = await this.getTotalFrames(tempFilePath);
+
+    const ffmpegProcess = spawn(ffmpegPath, [
+      `-i`,
+      `${tempFilePath}`,
+      "-vcodec",
+      "libx264",
+      "-b:v",
+      "200k",
+      "-c:a",
+      "aac",
+      "-vf",
+      `scale=-2:480`,
+      "-f",
+      "mp4",
+      `${saveFileTo}.mp4`,
+    ]);
+
+    ffmpegProcess.stderr
+      .pipe(progressStream(frames))
+      .on("data", (data) => console.log(`${data.progress.toFixed(2)}%`));
+
+    ffmpegProcess.stdout.on("end", () => {
+      ffmpegProcess.kill();
+      unlinkSync(tempFilePath);
+      onFinish();
+      console.log("File " + info.filename + " finished!");
+    });
+  }
+
+  private async getTotalFrames(file: string): Promise<number> {
+    const info = await ffprobe(file, { path: ffprobeStatic.path });
+    const videoStream = info.streams.find(
+      (stream) => stream.codec_type === "video"
+    );
+    // Get the total frame information from the nb_frames property
+    const totalFrames = videoStream.nb_frames;
+
+    return Number(totalFrames);
   }
 }
